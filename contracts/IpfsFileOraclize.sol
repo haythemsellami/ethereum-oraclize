9a9309be9ca405b669fb770801ee45df0c28443e
Haythem Sellami <sellamihaythem96@gmail.com>
2018-09-09 01:26:58 +0100

    check for valid query id

--- contracts/IpfsFileOraclize.sol
+++ contracts/IpfsFileOraclize.sol
@@ -6,17 +6,26 @@ import "./Oraclize.sol";
 contract IpfsFileOraclize is usingOraclize {
 
     string public fileContent;
-    bytes32 public oraclizeID;
+    bytes32 public oraclizeId;
+    mapping(bytes32=>bool) validOraclizeIds;
     
     constructor() public {
     }
 
     function getContent(string _fileHash) public payable {
-        oraclizeID = oraclize_query("IPFS", _fileHash);
+        oraclizeId = oraclize_query("IPFS", _fileHash);
+        validOraclizeIds[oraclizeId] = true;
     }
 
-    function __callback(bytes32 _oraclizeID, string _fileContent) public {
+    function __callback(bytes32 _oraclizeId, string _fileContent) public {
+        //check that the address sender is the oraclizer
         require(msg.sender == oraclize_cbAddress(), "Not authorized");
+        //check that query id is valid
+        require(validOraclizeIds[_oraclizeId] == true, "invalid oraclize id");
+
+        //Update file content
         fileContent = _fileContent;
+
+        delete validOraclizeIds[_oraclizeId];
     }
 }
